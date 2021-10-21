1681c52a0b698d087c0204ad57ade6638e6e70ed
ZakirbhaiBott <87867737+ZakirbhaiBott@users.noreply.github.com>
2021-10-22 02:13:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -48,20 +48,14 @@ PDiskBot = Client(
     bot_token=Configs.BOT_TOKEN
 )
 
-START_TEXT = str("Hi, I am Alive!\n
 
-🔰 How to Search in this Bot?\n\n
-
-Click Below Search Button And Type Series Name Season and Episode According To Examples And Send\n\n
-
-Examples-`\nMoney Heist S02E01``\nStranger things S01E04`")
 
 @PDiskBot.on_message(filters.command("start") & ~filters.edited)
 
 async def start_handler(_, m: Message):
     await m.reply_photo("https://telegra.ph/file/61a2dcd6c644b51e3f447.jpg",
 
-        caption=START_TEXT.format(update.from_user.first_name),
+        caption="Hi".format(update.from_user.first_name),
 
         reply_markup=InlineKeyboardMarkup([
 
