5c54198c6252b9550a7f9c8749a9f90fafad928a
ZakirbhaiBott <87867737+ZakirbhaiBott@users.noreply.github.com>
2021-10-22 19:00:22 +0530

    main.py

--- main.py
+++ main.py
@@ -90,9 +90,10 @@ async def text_handler(_, m: Message):
             if count > Configs.MAX_RESULTS:
                 break
             count += 1
-            text += f"**{data[i]['title']}**\n" \
-                    f"**PDisk Link:** {Configs.PDISK_DOMAIN + 'share-video?videoid=' + data[i]['share_link'].split('=', 1)[-1]}\n\n\n",                    
-        try: await editable.edit(text, disable_web_page_preview=True)        
+            text += f"**Title:** `{data[i]['title']}`\n" \
+                    f"**Description:** `{data[i]['description']}`\n" \
+                    f"**PDisk Link:** {Configs.PDISK_DOMAIN + 'share-video?videoid=' + data[i]['share_link'].split('=', 1)[-1]}\n\n"
+        try: await editable.edit(text, disable_web_page_preview=True)
         except MessageNotModified: pass
 
 
