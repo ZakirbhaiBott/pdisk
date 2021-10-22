2754bd4e9503e78c92a9d94fe890ac211c619516
ZakirbhaiBott <87867737+ZakirbhaiBott@users.noreply.github.com>
2021-10-22 19:23:31 +0530

    main.py

--- main.py
+++ main.py
@@ -91,7 +91,6 @@ async def text_handler(_, m: Message):
                 break
             count += 1
             text += f" **{data[i]['title']}**\n" \
-                   
                     f"**PDisk Link:** {Configs.PDISK_DOMAIN + 'share-video?videoid=' + data[i]['share_link'].split('=', 1)[-1]}\n\n"
         try: await editable.edit(text, disable_web_page_preview=True)
         except MessageNotModified: pass
